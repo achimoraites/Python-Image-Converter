3d5b9580f687e512a3aa98e893fe38e056ff600e
Achilles <achimoraites@yahoo.gr>
2017-03-20 16:09:19 +0200

    Added option to select filetype

--- app/convert.py
+++ app/convert.py
@@ -1,12 +1,13 @@
 from PIL import Image
 import os
 from threading import *
+import optparse
 
 # Keeping the output cleaned 
 screenLock = Semaphore(value=1)
 
 # set the extension of files to be converted
-fileExtension = '.IIQ'
+fileExtension = '.pdf'
 # where to save our images
 directory = "converted"
 # create a directory if needed to store our converted images
@@ -15,21 +16,47 @@ if not os.path.exists(directory):
 
 # convert function
 def convert_file(file,directory):
-	im = Image.open(file)
-	basewidth = 2048
-	wpercent = (basewidth/float(im.size[0]))
-	hsize = int((float(im.size[1])*float(wpercent)))
-	im = im.resize((basewidth,hsize), Image.ANTIALIAS)
-	im.save(directory +'/' + file + ".jpg", "JPEG")
-	screenLock.acquire()
-	print file + " Converted! \n"
-	screenLock.release()
+	try:
+		im = Image.open(file)
+		#basewidth = 2048
+		#wpercent = (basewidth/float(im.size[0]))
+		#hsize = int((float(im.size[1])*float(wpercent)))
+		#im = im.resize((basewidth,hsize), Image.ANTIALIAS)
+		im.save(directory +'/' + file + ".jpg", "JPEG",dpi=(600,600))
+		screenLock.acquire()
+		print file + " Converted! \n"
+		screenLock.release()
+	except:
+		pass
 
-# find files to convert
-for file in os.listdir("."):
-    if file.endswith(fileExtension):
-        #print(os.path.join("Converting :" + directory + '/', file))
-        # Added multithreds to complete conversion faster
-        t = Thread(target = convert_file, args = (file,directory))
-        t.start();
-        #convert_file(file)
+
+
+def main():
+	parser = optparse.OptionParser('### PYTHON IMAGE CONVERTER ### \n'+
+	'Place this script inside the folder you want to convert \n' +
+	"\n usage%prog "+ \
+	"-f <file type>" + '\n\n' + 'Convert all the .dng images example : \n\n convert.py -f .dng')
+	parser.add_option('-f', dest='fileType', type='string',\
+	help='specify file type of files to convert ex: .dng ')
+	(options, args) = parser.parse_args()
+	if (options.fileType == None):
+		print parser.usage
+		exit(0)
+	else:
+		fileType = options.fileType
+		
+	
+	# find files to convert
+	
+	
+		for file in os.listdir('.'):
+			if file.endswith(fileType.lower()) | file.endswith(fileType.upper()):
+				#print(os.path.join("Converting :" + directory + '/', file))
+				# Added multithreds to complete conversion faster
+				t = Thread(target = convert_file, args = (file,directory))
+				t.start();
+				#convert_file(file)
+			
+	
+if __name__ == '__main__':
+		main()
