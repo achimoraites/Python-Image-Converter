4360e9d4d28c47a1c471a2cd2fb0e1ef7dcd1381
Achilles <achimoraites@yahoo.gr>
2017-03-21 10:49:28 +0200

    Update!

--- README.md
+++ README.md
@@ -1,39 +1,32 @@
 # Python-Image-Converter
 
-### Not supported formats :
-CRW , PEF , ORF , CR2 , MRW .
+
 ## This is a simple Image converter written in Python
-The app is using PIL , you simply put the convert.py in the folder you want to convert set the options 
-and the app will convert all the selected files to jpeg . I wrote this app to convert multiple image files
+The app is using PIL ,rawpy and imageio you simply put the convert.py in the folder you want to convert set the options 
+and the app will convert all the selected files to png . I wrote this app to convert multiple image files
 as fast as possible.
+# Notes
+The .psd files are converted to .jpeg format!!! </br>
+
+
 ## What files can be used
-The app supports many image formats , except .raw files! </br>
+The app supports many image formats , EVEN RAW files! </br>
+# DNG , CR2 , NEF , ERF , ORF , PPM , MOS , MRF , MRW and more!!!
 Personally i convert a lot of .psd , .TIF and .dng files !!! </br>
-also you can use it to create thumbnails fast , if you have tons of files!
 ### Before you can use it Install Image
 
 </br> pip install Image 
+</br> pip install rawpy
+</br> pip install imageio 
 
 ### Example usage
 
 </br> open the command line and go to the folder containing images you want to convert:
-C:\Users\Toula\Desktop\Photos_for_conversion> </br>
-copy the convert.py in the folder </br>
-Edit the file to customise your conversion: </br>
-# set the extension of files to be converted 
-fileExtension = '.IIQ' </br>
-The extension sould be set to the filetype of images to be converted </br>
-in line 19 you will find : </br>
-basewidth = 2048 </br>
-Change it to set the image size ,
-if you want a thumbnail you can set it to 200 and the proportions are automatically calculated!</br>
-save your changes !!!
+C:\Users\User_NAME\Desktop\Photos_for_conversion> </br>
 
-
-### Now lets convert!
 Run the convert.py :  </br>
-C:\Users\Toula\Desktop\Photos_for_conversion>python convert.py </br>
-The application will create a folder converted where all your converted images are located!
+C:\Users\User_NAME\Desktop\Photos_for_conversion>python convert.py </br>
+The application will create a folder 'converted' where all your converted images are located!
 </br>
 And you are done! 
 
--- app/convert.py
+++ app/convert.py
@@ -1,22 +1,46 @@
 from PIL import Image
 import os
 from threading import *
-import optparse
+import rawpy
+import imageio
+from datetime import datetime
+
 
 # Keeping the output cleaned 
 screenLock = Semaphore(value=1)
 
 # set the extension of files to be converted
-fileExtension = '.pdf'
+#fileExtension = '.pdf'
 # where to save our images
 directory = "converted"
 # create a directory if needed to store our converted images
 if not os.path.exists(directory):
     os.makedirs(directory)
 
+# convert RAW function
+def convert_raw(file,directory):
+	#path = 'image.nef'
+	screenLock.acquire()
+	print datetime.now().time().strftime('%H:%M:%S') + " Raw conversion: " + file
+	screenLock.release()
+	try:
+		path = file
+		with rawpy.imread(path) as raw:
+			rgb = raw.postprocess()
+		imageio.imsave( directory +'\\' +file+'.png', rgb)
+		screenLock.acquire()
+		print datetime.now().time().strftime('%H:%M:%S') + " Converted:  " + directory +'\\' +file+'.png'
+		screenLock.release()
+		
+	except:
+		pass	
+
 # convert function
 def convert_file(file,directory):
 	try:
+		screenLock.acquire()
+		print datetime.now().time().strftime('%H:%M:%S') + " Converting : "+file+"\n"
+		screenLock.release()
 		im = Image.open(file)
 		#basewidth = 2048
 		#wpercent = (basewidth/float(im.size[0]))
@@ -24,39 +48,42 @@ def convert_file(file,directory):
 		#im = im.resize((basewidth,hsize), Image.ANTIALIAS)
 		im.save(directory +'/' + file + ".jpg", "JPEG",dpi=(600,600))
 		screenLock.acquire()
-		print file + " Converted! \n"
+		print 'Converted : ' + datetime.now().time().strftime('%H:%M:%S') + ' ' + file + " \n"
 		screenLock.release()
+	
 	except:
 		pass
-
-
-
-def main():
-	parser = optparse.OptionParser('### PYTHON IMAGE CONVERTER ### \n'+
-	'Place this script inside the folder you want to convert \n' +
-	"\n usage%prog "+ \
-	"-f <file type>" + '\n\n' + 'Convert all the .dng images example : \n\n convert.py -f .dng')
-	parser.add_option('-f', dest='fileType', type='string',\
-	help='specify file type of files to convert ex: .dng ')
-	(options, args) = parser.parse_args()
-	if (options.fileType == None):
-		print parser.usage
-		exit(0)
-	else:
-		fileType = options.fileType
 		
-	
+def main():
+	print '### PYTHON IMAGE CONVERTER ### \n Place this script inside the folder you want to convert \n'
+	# counter
+	file_counter = 0;	
+	print "Started conversion at : " + datetime.now().time().strftime('%H:%M:%S')+ '\n'
 	# find files to convert
 	
 	
-		for file in os.listdir('.'):
-			if file.endswith(fileType.lower()) | file.endswith(fileType.upper()):
+	for file in os.listdir('.'):
+		
+				
+			if file.endswith('.psd') | file.endswith('.psd'.upper()):
 				#print(os.path.join("Converting :" + directory + '/', file))
 				# Added multithreds to complete conversion faster
-				t = Thread(target = convert_file, args = (file,directory))
-				t.start();
+				t2 = Thread(target = convert_file, args = (file,directory))
+				t2.start();
+				file_counter += 1
+				
 				#convert_file(file)
-			
+			if file.endswith('.py') | os.path.isdir(file):
+				pass
+			else:
+				#convert_raw(file)
+				t = Thread(target = convert_raw, args = (file,directory))
+				t.start();
+				file_counter += 1
+				
+	screenLock.acquire()
+	print "\n\n Files Converting : " + str(file_counter) + '\n\n'
+	screenLock.release()
 	
 if __name__ == '__main__':
 		main()
