da10838062375c2eaffaa4c65b319e3f8898b3fd
Achilles Moraites <achimoraites@yahoo.gr>
2023-04-30 08:49:56 +0200

    fix: CVE-2022-45199
    Pillow before 9.3.0 allows denial of service via SAMPLESPERPIXEL.
    
    https://nvd.nist.gov/vuln/detail/CVE-2022-45199

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="raw-image-converter",
-    version="1.0.2",
+    version="1.0.3",
     description="Batch conversions of raw images",
     long_description=README,
     long_description_content_type="text/markdown",
@@ -27,7 +27,7 @@ setup(
     keywords='cli, converter, raw, images',
     packages=["raw_image_converter"],
     install_requires=["numpy==1.22.3", "rawpy==0.17.1",
-                      "imageio==2.16.2", "Pillow==9.2.0"],
+                      "imageio==2.16.2", "Pillow==9.3.0"],
     entry_points={
         "console_scripts": [
             "raw_image_converter=raw_image_converter.__main__:main",
